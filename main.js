2871b9d4b9ea31be8c67db3211ad871668917d51
quent043 <quent043@hotmail.com>
2020-04-27 19:57:08 +0200

    Petites corrections + Rectification d'un bug dans la boucle for

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- const SHA256 = require('crypto-js/sha256)');
+ const SHA256 = require('crypto-js/sha256');
 
 class Block {
     constructor (index, timestamp, data, previousHash =''){
@@ -6,16 +6,16 @@ class Block {
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = this.calculateHash;
+    this.hash = this.calculateHash();
     this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash+ this.timestamp + JSON.stringify(this.data).toString() + this.nonce);
+        return SHA256(this.index + this.previousHash+ this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
     mineBlock(difficulty){
-        while(this.hash.subString(0, difficulty) !== Array(difficulty + 1).join("0")){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
             // Va retourner un string fait de tous les éléments de l'Array séparés par "0". Vu que l'array 
             // est vide, ça va retourner juste des 0.
             this.nonce++; //On fait ça sinon si on ne change pas au moins une valeur dans le block ,le SHA
@@ -64,8 +64,9 @@ class Blockchain{
                 return false;
             }
 
-            return true;
+            
         }
+        return true;
     }
 }
 
@@ -73,6 +74,7 @@ class Blockchain{
 let virtualPognon = new Blockchain();
 virtualPognon.addBlock(new Block(1, "16/02/2018", { amount: 4 }));
 virtualPognon.addBlock(new Block(2, "30/06/2016", { amount: 16 }));
+virtualPognon.addBlock(new Block(3, "30/06/2016", { amount: 22 }));
 
 console.log('Is Blockchain valid? ' + virtualPognon.isChainValid());
 console.log(JSON.stringify(virtualPognon, null, 4));
@@ -81,6 +83,7 @@ virtualPognon.chain[1].data = { amount: 10000};
 virtualPognon.chain[1].hash = virtualPognon.chain[1].calculateHash();
 
 console.log('Is Blockchain valid? ' + virtualPognon.isChainValid());
+console.log(JSON.stringify(virtualPognon, null, 4));
 
 // On peut faire ça avec le dernier block par contre ? Vu qu'il n'y a pas de block suivant pour check le Hash
 // Oui on peut, et même à un block donné on peut le faire si on recalcule de Hash de tous les blocks suivants
